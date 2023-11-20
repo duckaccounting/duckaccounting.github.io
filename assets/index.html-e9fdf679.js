5a6be4c523a5ae919a8165ec304790d01461dce8
leo-schick <67712864+leo-schick@users.noreply.github.com>
2023-11-20 19:29:29 +0000

    Deploying to gh-pages from @ duckaccounting/duckaccounting.github.io@2587cc717bee3ac2000edb34a0b6ac7b9af41941 🚀

---
+++ assets/index.html-e9fdf679.js
@@ -0,0 +1,3 @@
+import{_ as e}from"./plugin-vue_export-helper-c27b6911.js";import{o as t,c as n,a as o,b as r}from"./app-e70bbb13.js";const c={},d=r("p",null,"Hier findest du die letzten Neuigkeiten über die Duck Accounting App.",-1);function a(i,s){return t(),n("div",null,[d,o(`
+TODO add more keyword generated text here
+`)])}const m=e(c,[["render",a],["__file","index.html.vue"]]);export{m as default};
