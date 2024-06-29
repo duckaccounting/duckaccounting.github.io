b104a0deb5e117b953a88c3d9dc7ed74847ff7b4
leo-schick <67712864+leo-schick@users.noreply.github.com>
2024-06-29 07:36:56 +0000

    Deploying to gh-pages from @ duckaccounting/duckaccounting.github.io@c515ea136a1ca891c1a0f2a910b3c0fde0c47e79 🚀

--- assets/index.html-8hnYRZSG.js
+++
@@ -1,3 +0,0 @@
-import{_ as e}from"./plugin-vue_export-helper-DlAUqK2U.js";import{o as t,c as n,a as o,b as r}from"./app-BRMsQbmr.js";const c={},d=r("p",null,"Hier findest du die letzten Neuigkeiten über die Duck Accounting App.",-1);function a(i,s){return t(),n("div",null,[d,o(`
-TODO add more keyword generated text here
-`)])}const m=e(c,[["render",a],["__file","index.html.vue"]]);export{m as default};
--- assets/index.html-D7kZlhC5.js
+++
@@ -1,3 +0,0 @@
-import{_ as e}from"./plugin-vue_export-helper-DlAUqK2U.js";import{o as t,c as o,a as n,b as a}from"./app-BRMsQbmr.js";const c={},r=a("p",null,"Here you get the latest updates about the Duck Accounting application.",-1);function s(d,_){return t(),o("div",null,[r,n(`
-TODO add more keyword generated text here
-`)])}const m=e(c,[["render",s],["__file","index.html.vue"]]);export{m as default};
